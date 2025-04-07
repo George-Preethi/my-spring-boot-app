e0e354ee828aa929e0508af400c71469ea0801cc
George-Preethi <nallamothupreethi@gmail.com>
2025-04-07 18:27:05 -0400

    Update MySpringBootAppApplication.java

--- MySpringBootAppApplication.java
+++ MySpringBootAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySpringBootAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootAppApplication.class, args);
+		SpringApplication.run(MySpringBootAppApplication.class, args);###
 	}
 
 }
